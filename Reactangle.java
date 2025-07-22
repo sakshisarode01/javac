04f7c685b8c3b63ab77b99ad255c02f57a9d412a
Sakshi sarode <sarodesakshi238@gmail.com>
2025-07-22 19:33:40 +0530

    Add files via upload

---
+++ Reactangle.java
@@ -0,0 +1,11 @@
+package area;
+
+public class Reactangle {
+public static void main(String[] args){
+	int length=1;
+	int width=5;
+	int area=length * width;
+	System.out.println("Area of rectangle:"+area);
+}
+}
+    
