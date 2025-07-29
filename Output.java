aa749686ce0d59163a25d66f632f3fdb52c6e5c3
Sakshi sarode <sarodesakshi238@gmail.com>
2025-07-29 17:23:17 +0530

    Add files via upload

---
+++ Output.java
@@ -0,0 +1,14 @@
+package add;
+
+public class Output {
+	  static int a = 10;
+	  static int b = 5;
+public static void main(String[] args) 
+{
+     System.out.println("Addition :" +(a+b));
+     System.out.println("Subtraction :" +(a-b));
+     System.out.println("Multiplication :" +(a*b));
+     System.out.println("Division :" +(a/b));
+     System.out.println("Module :" +(a%b));
+}
+}
