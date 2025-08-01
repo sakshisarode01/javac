3404b6e8c8d52aa4cf8d15d209ea01db0da180cb
Sakshi sarode <sarodesakshi238@gmail.com>
2025-08-01 16:54:36 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,21 @@
+package abc;
+
+public class Demo {
+	  Demo() {
+	        System.out.println("I am in Non-Parameterized contructor");
+	    }
+
+	    Demo(int a, String b) {
+	        System.out.println("I am in Parameterized contructor--:" + a + "," + b);
+	    }
+
+	    void display() {
+	    }
+
+	    public static void main(String[] args) {
+	        Demo d1 = new Demo();
+	        Demo d2 = new Demo(30, "SMBST");
+	        d1.display();
+	        d2.display();
+	}
+}
