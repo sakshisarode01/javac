e651523e3beb4cbad1d3e3299c1198f64f45486e
Sakshi sarode <sarodesakshi238@gmail.com>
2025-08-02 17:33:10 +0530

    Add files via upload

--- Method.java
+++ Method.java
@@ -1,28 +1,41 @@
 package method;
-class demo{
-	void greetUser() {
-		System.out.println("Welcome");
-	}
-	int square(int num) {
-		System.out.println("The square of the number is:"+(num*num));
-		return 0;
-	}
-	void showDetails(String name,int age) {
-		System.out.println("Your Name:"+name);
-		System.out.println("Your Age:"+age);
-	}
-double getPi() {
-	double pi=3.14159;
-		System.out.println("The value of Pi:"+pi);
-		return 0;
-	}
-}
+
+import java.util.Scanner;
+
 public class Method {
-	public static void main(String[] args) {
-		demo d1=new demo();
-		d1.greetUser();
-		d1.square(2);
-		d1.showDetails("Sakshi",20);
-		d1.getPi();
-		}
+    public static void main(String[] args) {
+    	        Scanner scanner = new Scanner(System.in);
+
+    	        System.out.print("Enter the number of elements in the array: ");
+    	        int n = scanner.nextInt();
+
+    	        int[] arr = new int[n];
+
+    	        System.out.println("Enter the elements of the array:");
+    	        for (int i = 0; i < n; i++) {
+    	            arr[i] = scanner.nextInt();
+    	        }
+
+    	        System.out.println("Prime numbers in the array:");
+    	        for (int num : arr) {
+    	            if (isPrime(num)) {
+    	                System.out.print(num + " ");
+    	            }
+    	        }
+    	    }
+
+    	    public static boolean isPrime(int num){
+    	        if (num <= 1) {
+    	            return false;
+    	        }
+    	        if(num <= 3) {
+    	        	return true;
+    	        }
+    	     
+    	         for(int i = 2; i <= num / 2; i++){
+    	            	if(num % i == 0)
+    		        return false;
+    	 }
+    	            return true;
+ }
 }
