159724c3922b0f555fdf53fb30458354a77c9e1b
Sakshi sarode <sarodesakshi238@gmail.com>
2025-08-07 18:31:19 +0530

    Add files via upload

---
+++ Table.java
@@ -0,0 +1,14 @@
+package table;
+import java.util.Scanner;
+public class Table {
+	 public static void main(String[] args) {
+	        for (int i = 1; i <= 15; i++) {
+	            System.out.println("Table of "+(i)+" number:\n");
+	            for (int j = 1; j <= 10; j++) {
+	                
+	                System.out.println(i + "*" + j + "=" +i*j);
+	            }
+	            System.out.println("----------------------");
+	 }
+	 }
+	 }
