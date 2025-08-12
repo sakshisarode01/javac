387d6ea1ea906cae12e9794e636e2843d7b6dc35
Sakshi sarode <sarodesakshi238@gmail.com>
2025-08-12 16:02:29 +0530

    Add files via upload

---
+++ Ope.java
@@ -0,0 +1,31 @@
+package ac;
+import java.util.Scanner;
+public class Ope {
+	 public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+
+	        System.out.println("Enter first number:");
+	        int no1 = sc.nextInt();
+
+	        System.out.println("Enter second number:");
+	        int no2 = sc.nextInt();
+
+	        System.out.println("First number is: " + no1);
+	        System.out.println("Second number is: " + no2);
+	        System.out.println("Addition is: " + (no1 + no2));
+	        System.out.println("Subtraction is: " + (no1 - no2));
+	        System.out.println("Multiplication is: " + (no1 * no2));
+	        System.out.println("Division is: " + (no1 / no2));
+	        System.out.println("Remainder is: " + (no1 % no2));
+
+	        if (no1 > no2 && no1 != no2) {
+	            System.out.println("First number is greater and both numbers are different");
+	        } else if (no1 < no2 && no1 != no2) {
+	            System.out.println("Second number is greater and both numbers are different");
+	        } else if (no1 == no2) {
+	            System.out.println("Both numbers are equal");
+	        }
+
+	        sc.close();
+}
+}
