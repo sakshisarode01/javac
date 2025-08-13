1ed0675c15f782c2bea57e9be2848ccee4e48be2
Sakshi sarode <sarodesakshi238@gmail.com>
2025-08-13 15:38:30 +0530

    Add files via upload

--- BankAcc.java
+++ BankAcc.java
@@ -1,21 +1,18 @@
-package bank;
+package abc;
+import java.util.Scanner;
 
 public class BankAcc {
-	static String BankName;
-
-    BankAcc(String AccHolder, double balance) {
-        System.out.println("Account Holder is: " + AccHolder);
-        System.out.println("Bank Balance is: " + balance);
-    }
-
-    void display() {
-        System.out.println("Bank name is: " + BankName);
-    }
-
-    public static void main(String[] args) 
-    {
-        BankAcc b1 = new BankAcc("sakshi", 2000.0);
-        BankAcc.BankName = "HDFC";
-        b1.display();
+static void ShowBankName(String BankName){
+		System.out.println("Bank Name is:"+BankName);
+	}
+void displayBalance(){
 }
+public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter your Bank Balance:");
+	float balance=sc.nextFloat();
+	System.out.println("Bank Balance:"+balance);
+	sc.close();
+	ShowBankName("HDFC");
+	}
 }
