767198d11caf2a90518d2b98a20d8c0b1c8a2942
Sakshi sarode <sarodesakshi238@gmail.com>
2025-08-13 14:04:40 +0530

    Add files via upload

---
+++ AppExecution.java
@@ -0,0 +1,11 @@
+package com.Final;
+
+public class AppExecution {
+public static void main(String[] args) {
+	System.out.println("App Running....");
+	}
+static
+{
+	System.out.println("App Starting....");
+}
+}
