5bae2757371804be336dee36f29c75a9e2ebee02
Sakshi sarode <sarodesakshi238@gmail.com>
2025-08-14 14:41:18 +0530

    Add files via upload

--- Demo.java
+++ Demo.java
@@ -1,22 +1,35 @@
-package constructor;
+package Demooo;
+import java.util.Scanner;
 
+class Product{
+	static String category;
+Product(int Id,String Name,float Price){
+		System.out.println("Product Id:"+Id);
+		System.out.println("Product Name:"+Name);
+		System.out.println("Product price:"+Price);
+		System.out.println("Product category:"+category);
+	}
+	static {
+		category="Electronics";
+	}
+	void display() {
+		System.out.println("Product Id is:");
+		System.out.println("Product Name is:");
+		System.out.println("Product price is:");
+		System.out.println("Product category:");
+	}
+}
 public class Demo {
-	Demo() {
-        System.out.println("I am in Non-Parameterized contructor");
-    }
-
-    Demo(int a, String b) {
-        System.out.println("I am in Parameterized contructor:-" + a + "," + b);
-    }
-
-    void display() {
-
-    }
-
-    public static void main(String[] args) {
-        Demo d1 = new Demo();
-        Demo d2 = new Demo(30, "SMBST");
-        d1.display();
-        d2.display();
+	public static void main(String[] args) {
+ Scanner sc=new Scanner(System.in);
+   System.out.println("Enter Product  Id:");
+   int Id=sc.nextInt();
+   System.out.println("Enter Product  Name:");
+   String Name=sc.next();
+   System.out.println("Enter Product  Price:");
+   float Price=sc.nextFloat();
+   Product p1=new Product(Id,Name,Price);
+p1.display();
+sc.close();
 }
 }
