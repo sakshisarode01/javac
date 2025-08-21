85a61fbe4d84e868e79ad1bffdbeb9d066bae935
Sakshi sarode <sarodesakshi238@gmail.com>
2025-08-21 14:42:17 +0530

    Add files via upload

--- Person.java
+++ Person.java
@@ -1,18 +1,16 @@
 package person;
 
 public class Person {
-	 Person(String name, int age) {
-	        System.out.println("Name of The Person is:" + name);
-	        System.out.println("Age of The Person is:" + age);
-	    }
-
-	    void display() {
-	    }
-
-	    public static void main(String[] args) {
-	        Person p1 = new Person("Sakshi", 20);
-	        Person p2 = new Person("Mansi", 19);
-	        p1.display();
-	        p2.display();
-	}
+   Person(String name,int age){
+	System.out.println("Name of The Person is:"+name);
+	System.out.println("Age of The Person is:"+age);
+}
+   void display() {
+   }
+   public static void main(String[] args) {
+	   Person p1=new Person("Aditi",20);
+	   Person p2=new Person("Mansi",19);
+       p1.display();
+       p2.display();
+   }
 }
