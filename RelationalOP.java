2bddb4e5bd42013ac98c8742be05aa37898362d3
Sakshi sarode <sarodesakshi238@gmail.com>
2025-08-22 20:56:19 +0530

    Add files via upload

---
+++ RelationalOP.java
@@ -0,0 +1,15 @@
+package abc;
+
+public class RelationalOP {
+	static int a=20;
+	static int b=21;
+	public static void main(String[] args)
+	{
+		System.out.println("a is less than b:"+(20<21));
+		System.out.println("a is greater than b:"+(20>21));
+		System.out.println("a is greater than and equal to:"+(20>=21));
+		System.out.println("a is less than and equal to:"+(20<=21));
+		System.out.println("a is equal to:"+(20==21));
+		System.out.println("a is not equal to:"+(20!=21));	
+	}
+}
