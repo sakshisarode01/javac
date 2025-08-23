9a381420bf3434a12af31c0aa61d2a531cb0c518
Sakshi sarode <sarodesakshi238@gmail.com>
2025-08-23 18:50:05 +0530

    Add files via upload

---
+++ Chain3.java
@@ -0,0 +1,15 @@
+package abc;
+
+public class Chain3 {
+	 public static void main(String[] args) {
+	        int a, b, c;
+	        a = b = c = 100;
+	        System.out.println("a: " + a);
+	        System.out.println("b: " + b);
+	        System.out.println("c: " + c);
+	        b=10;
+	        System.out.println("a: " + a);
+	        System.out.println("b: " + b);
+	        System.out.println("c: " + c);
+	    }
+	}
