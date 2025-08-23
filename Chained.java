8248b54e94426e54904500b78d4318ed4808559a
Sakshi sarode <sarodesakshi238@gmail.com>
2025-08-23 18:49:52 +0530

    Add files via upload

---
+++ Chained.java
@@ -0,0 +1,11 @@
+package abc;
+
+public class Chained {
+	public static void main(String[] args) {
+        int student1, student2, student3; 
+        student1 = student2 = student3 = 50;
+        System.out.println("Student 1 Score: " + student1);
+        System.out.println("Student 2 Score: " + student2);
+        System.out.println("Student 3 Score: " + student3);
+    }
+}
