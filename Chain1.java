33ff16c69672cc102b22ebe9eae3c4b2fa859086
Sakshi sarode <sarodesakshi238@gmail.com>
2025-08-23 18:50:20 +0530

    Add files via upload

---
+++ Chain1.java
@@ -0,0 +1,12 @@
+package abc;
+
+public class Chain1 {
+	public static void main(String[] args) {
+        int emp1, emp2, emp3;
+        emp1 = emp2 = emp3 = 30000; 
+        emp2 += 5000; 
+        System.out.println("Employee 1 Salary: " + emp1);
+        System.out.println("Employee 2 Salary: " + emp2);
+        System.out.println("Employee 3 Salary: " + emp3);
+    }
+}
